91deafcb583555060360c1a202d2eb8cef30fbe1
James McDonald <james_mcd98@hotmail.com>
2020-02-01 00:28:01 -0500

    Creaetd recipe search route

--- server/recipe_search.py
+++ server/recipe_search.py
@@ -1,14 +1,18 @@
 import requests
 from flask import request
 
+#In: payload with any combination of: query(name), cuisine, diet, intolerences
+#Out: list of recipies that match the parameters
 def get_recipes():
 
     apiKey = "07712accff1746d6a80e64c71cf4a73e"
-    query = "Pasta"
 
-    url = "https://api.spoonacular.com/recipes/search?apiKey={}&query={}&instructionsRequired=true".format(apiKey,query)
+    query = request.form.get('query', '')
+    cuisine = request.form.get('cuisine', '')
+    diet = request.form.get('diet', '')
+    intolerences = request.form.get('intolerences', '')
 
-    
+    url = "https://api.spoonacular.com/recipes/search?apiKey={}&query={}&cuisine={}&diet={}&intolerences={}&instructionsRequired=true".format(apiKey,query,cuisine,diet,intolerences)
 
     payload = {}
     headers= {}
