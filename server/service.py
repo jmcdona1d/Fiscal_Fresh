bcec70b51f9612e979a4d220337d6422a037c734
Michael Dowling <15mhd@queensu.ca>
2020-01-31 22:17:33 -0500

    set up server side routing

--- server/service.py
+++ server/service.py
@@ -1,5 +1,26 @@
-from flask import Flask, request
+from flask import Flask
 app = Flask(__name__)
+
+@app.route('/get_recipes')
+def get_recipes():
+    # call James' code here
+    return "a"
+
+@app.route('/order')
+def order():
+    # call Michael's code here
+    return "a"
+
+@app.route('/authenticate')
+def sign_in():
+    # authenticate, return jwc token
+    return "a"
+
+@app.route('/recipe_history')
+def get_recipe_history():
+    # get recipe history for authenticated user
+    return "a"
+
 
 @app.route('/')
 def hello():
