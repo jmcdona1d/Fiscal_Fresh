8b27abe9a5b064a4f0ae016866902b6ab6a61bb5
Michael Dowling <15mhd@queensu.ca>
2020-02-01 11:59:30 -0500

    updating routing for recipe history

--- server/service.py
+++ server/service.py
@@ -17,21 +17,10 @@ def order_ingredients():
     # call code to order ingredients passed with the request
     return order()
 
-@app.route('/authenticate')
-def sign_in():
-    # authenticate, return jwc token
-    return "a"
-
 @app.route('/recipe_history')
-def get_recipe_history():
+def get_user_recipe_history():
     # get recipe history for authenticated user
-    return "a"
-
-
-@app.route('/')
-def hello():
-    return "hello"
-
+    return get_recipe_history()
 
 if __name__ == "__main__":
     app.run(debug=True)
